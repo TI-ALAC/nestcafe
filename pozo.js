28639db69d8e53346452d41781af70b48af0a0d6
Luis Toledo <dti@alacoohperu.pe>
2022-08-02 12:40:29 -0500

    cambios en logica final

--- pozo.js
+++ pozo.js
@@ -6,12 +6,6 @@ var content5 = document.getElementById('contenido5');
 
 async function init() {
 
-  var options = {
-    enableHighAccuracy: true,
-    timeout: 5000,
-    maximumAge: 0
-  };
-
   function success(pos) {
     var crd = pos.coords;
     localStorage.setItem("lon", crd.longitude);
@@ -24,50 +18,32 @@ async function init() {
 
   navigator.geolocation.getCurrentPosition(success, error, options);
 
-  var latitud = localStorage.getItem("lon");
-  console.log(latitud)
-  var longitud = localStorage.getItem("lat");
-  console.log(longitud)
-  const url = `http://localhost:5023/api/clima/${latitud}/${longitud}`;
-  console.log(url)
-  const response = await axios.get(`http://localhost:5023/api/clima/${latitud}/${longitud}`);
-  console.log(response)
 
-  //const lat = sessionStorage.getItem('lat');
-  //const long = sessionStorage.getItem('long');
-  //await getUser();
-  const text_clima = response.data.data;
-  //var cli = localStorage.getItem('climaFinal');
-  console.log("text_clima", text_clima)
-  if (text_clima == 'muy nuboso') {
-    console.log("ingresando 1")
-    content.style.display = "block";
-  } else if (text_clima == 'cielo claro' || text_clima == 'nubes dispersas' || text_clima == 'algo de nubes') {
-    console.log("ingresando 2")
-    content2.style.display = "block";
-  } else if (text_clima == 'nubes') {
-    console.log("ingresando 3")
-    content3.style.display = "block";
-  } else if (text_clima == 'niebla') {
-    console.log("ingresando 4")
-    content4.style.display = "block";
-  } else if (text_clima == 'lluvia ligera' || text_clima == 'tormenta con lluvia ligera') {
-    console.log("ingresando 5")
-    content5.style.display = "block";
-  }
+
+  setTimeout(function () {
+    var latitud = localStorage.getItem("lon");
+    var longitud = localStorage.getItem("lat");
+    const response = await axios.get(`http://localhost:5023/api/clima/${latitud}/${longitud}`);
+    console.log(response)
+    const text_clima = response.data.data;
+    if (text_clima == 'muy nuboso') {
+      console.log("ingresando 1")
+      content.style.display = "block";
+    } else if (text_clima == 'cielo claro' || text_clima == 'nubes dispersas' || text_clima == 'algo de nubes') {
+      console.log("ingresando 2")
+      content2.style.display = "block";
+    } else if (text_clima == 'nubes') {
+      console.log("ingresando 3")
+      content3.style.display = "block";
+    } else if (text_clima == 'niebla') {
+      console.log("ingresando 4")
+      content4.style.display = "block";
+    } else if (text_clima == 'lluvia ligera' || text_clima == 'tormenta con lluvia ligera') {
+      console.log("ingresando 5")
+      content5.style.display = "block";
+    }
+  }, 2000);
 
 }
 
 init();
-
-
-async function getUser() {
-  try {
-    const response = await axios.get('http://localhost:5023/api/clima');
-    const text_clima = response.data.data;
-    localStorage.setItem('climaFinal', JSON.stringify(text_clima));
-    const climaFinal = JSON.parse(localStorage.getItem('climaFinal'));
-    console.log(climaFinal)
-  } catch (error) {
-  }
-}
