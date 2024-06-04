eab3c136f2758bb2a02b2def3cdd0c775894ef19
Luis Toledo <luis@hotmail.com>
2024-06-04 14:04:45 -0500

    cambio clima

--- pozo.js
+++ pozo.js
@@ -20,10 +20,11 @@ async function init() {
   const coord = await axios.get(`https://apialacooh.alacoohecuador.com/playlist/panel/${idPanel}`);
   var latitud = coord.data.data[0].point.coordinates[0];
   var longitud = coord.data.data[0].point.coordinates[1];
-  const response = await axios.get(`https://weatherstation.alacoohperu.pe/api/clima/${latitud}/${longitud}`);
+  //const response = await axios.get(`https://weatherstation.alacoohperu.pe/api/clima/${latitud}/${longitud}`);
   //const response2 = await axios.get(`https://weatherstation.alacoohperu.pe/api/climagrados/${latitud}/${longitud}`);
-  const text_clima = response.data.data.weather[0].description;
-  const datatemp = response.data.data.main.temp.toFixed(0);
+  // const text_clima = response.data.data.weather[0].description;
+  const text_clima = 'nubes';
+  // const datatemp = response.data.data.main.temp.toFixed(0);
   const result = datatemp.toString();
   const fecha = new Date();
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -33,7 +34,8 @@ async function init() {
   const palabraFecha = palabras[1];
   const unir = palabraDia + "," + palabraFecha;
   document.getElementById('date').innerHTML = unir;
-  document.getElementById('title1').innerHTML = result+'°';
+  // document.getElementById('title1').innerHTML = result+'°';
+  document.getElementById('title1').innerHTML = '21°';
 
   if (text_clima == 'nubes'  || text_clima == 'niebla' || text_clima == 'muy nuboso' ) {
     content3.style.display = "block";
